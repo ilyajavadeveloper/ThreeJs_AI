b5793846f57e6c283f10878bb157d78449a4abdd
Your Name <you@example.com>
2026-06-21 11:34:30 +0300

    HOME page + Custom Button

---
+++ src/components/AIPicker.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AIPicker = () => {
+    return (
+        <div>AIPicker</div>
+    )
+}
+export default AIPicker
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import CustomButton from './CustomButton';
+import AIPicker from './AIPicker.jsx';
+import ColorPicker from './ColorPicker.jsx';
+import FilePicker from './FilePicker.jsx';
+import Tab from './Tab.jsx';
 
 
-export { CustomButton };
+export { CustomButton,AIPicker,ColorPicker,FilePicker,Tab };
